8decc2d802b6faa7f2eead1d21014692ece37e3c
Kim Young Wan <powerkyw59@naver.com>
2020-10-29 22:48:23 +0900

    Canvas 화면전환 추가
    버튼을 통한 start->playing으로의 canvas전환 playing으로 넘어가면서 게임이 시작될 예정

---
+++ Kingdom Run/Assets/Scenes/KindomRun/Scripts/NavigationalCanvas.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationalCanvas : MonoBehaviour
+{
+    [SerializeField] private string _canvasName;
+    public string CanvasName => _canvasName;
+   
+}
---
+++ Kingdom Run/Assets/Scenes/KindomRun/Scripts/NavigationalCanvasManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using KPU;
+using UnityEditor;
+using System.Xml;
+using System.Linq;
+
+namespace Scene.Ui
+{
+    public class NavigationalCanvasManager : SingletonBehaviour<NavigationalCanvasManager>
+    {
+        private List<NavigationalCanvas> _navigationalCanvases;
+        private void Awake()
+        {
+            _navigationalCanvases = GetComponentsInChildren<NavigationalCanvas>(includeInactive : true).ToList();
+            //                                                                  ㄴ> true로 비활성화된 오브젝트도 가져옴
+        }
+        public void ShowCanvas(string canvasName)
+        {
+            var foundedCanvas = _navigationalCanvases.FirstOrDefault(c => c.CanvasName == canvasName);
+            if(foundedCanvas != null)
+            {
+                _navigationalCanvases.Remove(foundedCanvas);
+                _navigationalCanvases.Add(foundedCanvas);
+
+                for(var i = 0; i< _navigationalCanvases.Count;++i)
+                {
+                    var naviagationalCanvas = _navigationalCanvases[i];
+                    var canvasComponent = naviagationalCanvas.GetComponent<Canvas>();
+
+                    naviagationalCanvas.gameObject.SetActive(true);
+                    canvasComponent.overrideSorting = true;
+                    canvasComponent.sortingOrder = i;
+                    naviagationalCanvas.gameObject.SetActive(false);
+                }
+
+                foundedCanvas.gameObject.SetActive(true);
+            }
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
