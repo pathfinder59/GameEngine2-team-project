28603144c11e6eed07b070c348b451df4b79076b
Kim Young Wan <powerkyw59@naver.com>
2020-11-03 23:40:07 +0900

    폴더 정리
    폴더 정리

---
+++ Kingdom Run/Assets/Scenes/KindomRun/Scripts/Ui/StartCanvasUi.cs
@@ -0,0 +1,16 @@
+﻿using Scene.Ui;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scene.Ui
+{
+    public class StartCanvasUi : MonoBehaviour
+    {
+
+        public void ShowPlayingCanvas()
+        {
+            NavigationalCanvasManager.Instance.ShowCanvas("Playing");
+        }
+    }
+}
