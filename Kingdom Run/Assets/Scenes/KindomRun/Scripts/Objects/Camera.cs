9d390bd97e7a2ed0536d5d5e0571e60edc5c2f46
Kim Young Wan <powerkyw59@naver.com>
2020-11-08 23:40:23 +0900

    카메라 워킹을 위한 스크립트 작성
    체크포인트들을 자식으로 담고 있는 게임 오브젝트를 멤버로 받아
    순서대로 목표 체크포인트를 향해 일정한 속도로 이동합니다.

--- Kingdom Run/Assets/Scenes/KindomRun/Scripts/Objects/Camera.cs
+++ Kingdom Run/Assets/Scenes/KindomRun/Scripts/Objects/Camera.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Camera : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] GameObject checkpoints;
+    int nCheckPoint;
+    int currentCheckPoint;
+    //[SerializeField] List<GameObject> checkpointList;
     void Start()
     {
-        
+        nCheckPoint = checkpoints.transform.childCount;
+        currentCheckPoint = 0;
+
+        transform.eulerAngles = new Vector3(64.045f, - 0.104f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(currentCheckPoint < nCheckPoint)
+        {
+            Transform target = checkpoints.transform.GetChild(currentCheckPoint);
+
+            transform.position = Vector3.MoveTowards(transform.position, target.position, 4f * Time.deltaTime);
+
+            if (transform.position == target.position)
+                currentCheckPoint++;
+        }
     }
 }
