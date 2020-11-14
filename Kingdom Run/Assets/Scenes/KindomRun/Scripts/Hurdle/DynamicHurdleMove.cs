e040dd42aca9ac92d380757e23256813d3e04bf9
Min Hye Won <mwon0326@gmail.com>
2020-11-15 00:35:35 +0900

    움직이는 장애물 이동 구현

---
+++ Kingdom Run/Assets/Scenes/KindomRun/Scripts/Hurdle/DynamicHurdleMove.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Assets.Scenes.KindomRun.Scripts.Hurdle
+{
+    public class DynamicHurdleMove : MonoBehaviour
+    {
+        [SerializeField] private float speed = 0.05f;
+        [SerializeField] private GameObject target;
+        private Vector3 originalPos;
+        private Vector3 targetPos;
+        private bool isArrive = false;
+        private Animator _animator;
+        
+        private void Awake()
+        {
+            originalPos = this.transform.position;
+            targetPos = target.transform.position;
+            _animator = GetComponentInChildren<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!isArrive)
+            {
+                transform.LookAt(targetPos);
+                transform.position = Vector3.MoveTowards(transform.position, 
+                    targetPos, speed);
+
+                if (Vector3.Distance(transform.position, targetPos) <= 0.1f)
+                {
+                    isArrive = true;
+                }
+            }
+            else
+            {
+                target.transform.position = originalPos;
+                originalPos = transform.position;
+                targetPos = target.transform.position;
+                isArrive = false;
+            }
+
+            _animator.SetFloat("Speed", Vector3.Magnitude(transform.position));
+        }
+    }
+}
