68b6e77378048bc90f4ce2abe47980f7f1f0ee13
Min Hye Won <mwon0326@gmail.com>
2020-11-23 19:29:48 +0900

    적 애니메이션 추가

--- Kingdom Run/Assets/Scenes/KindomRun/Scripts/Objects/Enemy.cs
+++ Kingdom Run/Assets/Scenes/KindomRun/Scripts/Objects/Enemy.cs
@@ -7,6 +7,8 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     private NavMeshAgent _agent;
+
+    private Animator _animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +19,12 @@ public class Enemy : MonoBehaviour
     private void OnEnable()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponentInChildren<Animator>();
     }
     // Update is called once per frame
     void Update()
     {
+        _animator.SetFloat("Speed", _agent.speed);
     }
     public void ForwardTarget(object obj)
     {
