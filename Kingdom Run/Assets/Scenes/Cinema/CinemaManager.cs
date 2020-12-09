ceb9c3753a846201f725a47b013950f5ead8f5a1
Kim Young Wan <powerkyw59@naver.com>
2020-12-09 12:53:01 +0900

    씨네마씬 준비중
    준비중

---
+++ Kingdom Run/Assets/Scenes/Cinema/CinemaManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CinemaManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] PlayableDirector playableDirector;
+    private void Update()
+    {
+        if(playableDirector.time == 0)
+        {
+
+        }
+    }
+}
